e3f7b562e4711b74d538cfd30f6ddaaf21e1b040
xel <xeloronkali@gmail.com>
2022-03-05 22:26:52 +0100

    cc

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: xel <xel@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 16:11:49 by xel               #+#    #+#             */
-/*   Updated: 2022/03/01 16:31:02 by xel              ###   ########.fr       */
+/*   Updated: 2022/03/05 22:26:09 by xel              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,16 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
-	size_t	j;
 
-	j = 0;
 	i = 0;
-	while (src[j])
-		j++;
 	if (size != 0)
 	{
-		while (src[i] && i < size - 1)
+		while (src[i] && i < (size - 1))
 		{
 			dst[i] = src[i];
 			i++;
 		}
 		dst[i] = '\0';
 	}
-	return (j);
+	return (ft_strlen(src));
 }
